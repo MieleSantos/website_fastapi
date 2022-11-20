38e228de660e770cafc30ca0977017385a8179ca
mielesantos <mielesnts@gmail.com>
2022-11-20 20:28:11 -0300

    adiconando templates jinja

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def index():
+async def index(request: Request, usuario: str = "Felicity JOnes"):
+    context = {"request": request, "usuario": usuario}
 
-    conteudo = """
-        <center>
-            <h1>FastApi web na  <u>Geek University</u> </h1>
-            <spam>Para mais cursos, visite nosso site clicando
-            <a href="https://geekuniversity.com.br"
-            target="_blank">aqui</a> </spam>
-        </center>
-    """
-    return HTMLResponse(conteudo)
+    return templates.TemplateResponse("index.html", context=context)
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(
+        app="main:app", host="0.0.0.0", reload=True, port=8000, log_level="info"
+    )
